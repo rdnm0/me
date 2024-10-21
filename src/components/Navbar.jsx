88cbb94ce4ea82f7ccc6e5fd5780d64287145c19
rdnm0 <rohandhanam@gmail.com>
2024-10-20 22:07:00 -0400

    phone changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import DarkModeToggle from './DarkModeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 flex flex-wrap justify-between items-center p-4 sm:p-6 bg-white dark:bg-gray-800 shadow-md text-lg z-50">
-      <a href="#home" className="text-3xl font-bold text-gray-900 dark:text-white hover:animate-spin">
+    <nav className="fixed top-0 left-0 right-0 flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 bg-white dark:bg-gray-800 shadow-md text-lg z-50">
+      <a href="#home" className="text-3xl font-bold text-gray-900 dark:text-white hover:animate-spin mb-2 sm:mb-0">
         RD.
       </a>
       <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-8 mt-4 sm:mt-0">
-        <a href="#about" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out">
+        <a href="#about" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out text-xl">
           About
         </a>
-        <a href="#work" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out">
+        <a href="#work" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out text-xl">
           Work
         </a>
-        <a href="#resume" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out">
+        <a href="#resume" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out text-xl">
           Resume
         </a>
         <DarkModeToggle />
