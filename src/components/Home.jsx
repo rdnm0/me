427a023303250ec726f665b223949d5bf1d0bf61
rdnm0 <rohandhanam@gmail.com>
2024-10-20 21:37:25 -0400

    misc hcnages'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center px-4 dark:bg-gray-900">
-      <h1 className="text-6xl font-bold mb-4 text-gray-900 dark:text-white">Hey, I'm Rohan 👋</h1>
+      <h1 className="text-6xl font-bold mb-4 text-gray-900 dark:text-white">Hey, I'm Rohan. 👋</h1>
       <p className="text-xl text-gray-600 dark:text-gray-400 mb-12 max-w-2xl">
         I'm a product designer based in Toronto who enjoys breaking down complex problems
         to craft solutions that make it simpler for people to achieve their goals.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 flex flex-wrap justify-between items-center p-4 sm:p-6 bg-white dark:bg-gray-800 shadow-md text-lg z-50">
       <a href="#home" className="text-3xl font-bold text-gray-900 dark:text-white hover:animate-spin">
-        RD
+        RD.
       </a>
       <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-8 mt-4 sm:mt-0">
         <a href="#about" className="text-gray-700 dark:text-gray-300 font-semibold hover:text-black dark:hover:text-white transform hover:scale-110 transition duration-300 ease-in-out">
